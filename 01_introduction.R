fb1980a4523c07f0fef98d537204eaff63c17bf9
EconMaett <matthias.spichiger@bluewin.ch>
2024-01-18 14:39:31 +0100

    first commit

---
+++ 01_introduction.R
@@ -0,0 +1,17 @@
+# tidyquant ----
+
+
+## Vignettes ----
+
+# Core functions in tidyquant
+# https://cloud.r-project.org/web/packages/tidyquant/vignettes/TQ01-core-functions-in-tidyquant.html
+
+# Performance Analysis with tidyquant
+# https://cloud.r-project.org/web/packages/tidyquant/vignettes/TQ05-performance-analysis-with-tidyquant.html
+
+# Scaling and modeling with tidyquant
+# https://cloud.r-project.org/web/packages/tidyquant/vignettes/TQ03-scaling-and-modeling-with-tidyquant.html
+
+# Quantitative analysis
+# https://cloud.r-project.org/web/packages/tidyquant/vignettes/TQ02-quant-integrations-in-tidyquant.html
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# tidyquant
+
+The [tidyquant](https://business-science.github.io/tidyquant/) package is available on [CRAN](https://cran.r-project.org/package=tidyquant) and [GitHub](https://github.com/business-science/tidyquant/).
+
+## Citation
+
+Dancho M, Vaughan D (2024). tidyquant: Tidy Quantitative Financial Analysis. R package version 1.0.7.9000, https://github.com/business-science/tidyquant, https://business-science.github.io/tidyquant/. 
